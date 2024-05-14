7774229fc3d2cf3755bcf51b8eaac6d814ec6596
Aleksey <lexa.antonov-alex@yandex.ru>
2024-05-14 16:33:23 +0300

    Initial commit

---
+++ Assets/Scripts/GameSystem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+
+public class GameSystem : MonoBehaviour
+{
+    private InputController _inputController;
+    
+    private void Start()
+    {
+        _inputController = new InputController();
+    }
+
+    private void Update()
+    {
+        _inputController.Update();
+    }
+}
